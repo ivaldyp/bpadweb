45e76abec358c195ad2481ec3a23d12bb8725c76
ivaldyp <vivaaldy@gmail.com>
2020-02-07 09:49:55 +0700

    tau dah

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,11 @@
-@extends('layout.master')
+@extends('layouts.master')
 
 @section('content')
 
 @if($lelang['suspend'] == 'Y')
 <div id="ads" class="bottom pull-right">
 	<a href="">
-		<img src="{{ ('/mockup-v1/public/img/photo/lelang-bpad.jpeg') }}">
+		<img src="{{ ('/bpadweb/public/img/photo/lelang-bpad.jpeg') }}">
 		<h1 class="text-center" id="lelang-text">LELANG</h1>
 		<!-- <a href="#">Home</a> -->
 	</a>
@@ -22,7 +22,7 @@
 			<!-- <h1 class="title"><span style="background: linear-gradient(90deg, #0052D4 0%, #65C7F7 30%, #65C7F7 60%, #0052D4 100%); -webkit-background-clip: text;-webkit-text-fill-color: transparent; font-size: 64px; font-weight: bold;">BERITA TERKINI</span></h1> -->
 
 			<!-- <div style="text-align: center;">
-				<img height="200" src="{{ ('/mockup-v1/public/img/photo/bpad-logo-04b.png') }}">
+				<img height="200" src="{{ ('/bpadweb/public/img/photo/bpad-logo-04b.png') }}">
 			</div> -->
 
 			<!-- <h1 class=" chrome">BERITA</h1> -->
@@ -33,7 +33,7 @@
 </div>
 <div id="testimonial" class="section" style="height: 780px;">
 	<!-- background section -->
-	<div class="section-bg" style="background-image: url('{{ ('/mockup-v1/public/img/photo/background-00b.jpg')}}');">
+	<div class="section-bg" style="background-image: url('{{ ('/bpadweb/public/img/photo/background-00b.jpg')}}');">
 	</div>
 	<!-- /background section -->
 
@@ -188,7 +188,7 @@
 						<div class="article-img">
 							<a href="JavaScript:void(0);">
 								<span class="text-test" style="">
-							        <img style="width: 50px" src="{{ ('/mockup-v1/public/img/photo/bpad-logo-00.png') }}">
+							        <img style="width: 50px" src="{{ ('/bpadweb/public/img/photo/bpad-logo-00.png') }}">
 							        <hr>
 							        <h4 style="color: white">{{ $photo['judul'] }}</h4>
 							    </span>
@@ -249,14 +249,14 @@
 	<!-- home item -->
 	<div class="home-item"  style="overflow: hidden; height: 500px">
 		<!-- section background -->
-		<div class="section-bg" style="background-image: url('{{ ('/mockup-v1/public/img/slides/bg-2-2.jpg') }}'); background-size: contain; background-repeat: no-repeat;"></div>
+		<div class="section-bg" style="background-image: url('{{ ('/bpadweb/public/img/slides/bg-2-2.jpg') }}'); background-size: contain; background-repeat: no-repeat;"></div>
 		<!-- /section background -->
 	</div>
 	<!-- /home item -->
 	<!-- home item -->
 	<div class="home-item"  style="overflow: hidden; height: 500px">
 		<!-- section background -->
-		<div class="section-bg" style="background-image: url('{{ ('/mockup-v1/public/img/slides/bg-3-2.jpg') }}'); background-size: contain; background-repeat: no-repeat;"></div>
+		<div class="section-bg" style="background-image: url('{{ ('/bpadweb/public/img/slides/bg-3-2.jpg') }}'); background-size: contain; background-repeat: no-repeat;"></div>
 		<!-- /section background -->
 	</div>
 	<!-- /home item -->	
